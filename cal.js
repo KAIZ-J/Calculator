cb6fe3a0e003a0df4e411799abe1368d0c2db000
atom-x <faizjemal49@gmail.com>
2025-08-09 21:51:10 +0300

    summary

--- cal.js
+++ cal.js
@@ -1,12 +1,28 @@
 const result = document.getElementById("display");
-
+  let allowed = [0,1,2,3,4,5,6,7,8,9,"+","-","/","*","."]
+document.addEventListener("keypress",function(e){
+  if(allowed.some(el=>parseInt(e.key)===el) || allowed.some(el=>e.key===el)){
+add(e.key)
+  }
+})
+document.addEventListener("keydown",function(e){
+ if(e.key==="Backspace"){
+del()
+ }
+ else if(e.key==="Enter"){
+  calculate()
+ }
+ else if(e.key==="Delete"){
+  remove()
+ }
+})
 function add(input) {
   result.value += input;
 }
 
 function calculate() {
   try {
-    result.value = eval(result.value).toFixed(2);
+    result.value = eval(result.value)-Math.floor(eval(result.value))!==0?eval(result.value).toFixed(2):eval(result.value);
   } catch (error) {
     result.value = "error❌";
   }
